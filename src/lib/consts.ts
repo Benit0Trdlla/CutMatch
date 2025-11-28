805f9e2d34864e2a81b3407f855abb65e68f45c4
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2025-11-28 10:29:29 -0300

    feat: added dictionary.

---
+++ src/lib/consts.ts
@@ -0,0 +1,38 @@
+export const LANGUAGES = {
+    EN: {
+        HEADER: {
+            HEADER_SUBTITLE: 'The cut that suits you.',
+        },
+        DRAG_AND_DROP: {
+            TITLE: 'Upload your images',
+            HOW_TO_UPLOAD_TEXT: 'Drag and drop or click to upload images',
+            ALLOWED_IMAGE: 'Accepts image/* between 1.00KB and 10.00MB'
+        },
+        ADVANCED_OPTIONS: {
+            TITLE: 'Advanced options',
+            INPUT_GENRE: {
+                LABEL: 'Genre',
+                PLACEHOLDER: ['Male', 'Female'],
+            },
+            INPUT_AGE: 'Age',
+        }
+    },
+    ES: {
+        HEADER: {
+            HEADER_SUBTITLE: 'El corte que encaja contigo.',
+        },
+        DRAG_AND_DROP: {
+            TITLE: 'Sube tus fotos',
+            HOW_TO_UPLOAD_TEXT: 'Arrastra y suelta o haz clic para subir fotos',
+            ALLOWED_IMAGE: 'Acepta imágenes/* entre 1.00KB y 10.00MB'
+        },
+        ADVANCED_OPTIONS: {
+            TITLE: 'Opciones avanzadas',
+            INPUT_GENRE: {
+                LABEL: 'Género',
+                PLACEHOLDER: ['Hombre', 'Mujer'],
+            },
+            INPUT_AGE: 'Edad',
+        }
+    }
+} as const
