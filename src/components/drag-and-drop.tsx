f7a90889a6310896a32e617e38eafaf5b72b0bd1
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2025-12-01 10:56:37 -0300

    chore: custom empty state, drag-and-drop.

--- src/components/drag-and-drop.tsx
+++ src/components/drag-and-drop.tsx
@@ -1,8 +1,11 @@
 'use client';
 import { Dropzone, DropzoneContent, DropzoneEmptyState } from '@/components/ui/shadcn-io/dropzone';
+import { UploadIcon } from 'lucide-react';
 import { useState } from 'react';
+import { useTranslation } from '@/hooks/useTranslation';
 const DragAndDrop = () => {
     const [files, setFiles] = useState<File[] | undefined>();
+    const { t } = useTranslation();
     const handleDrop = (files: File[]) => {
         console.log(files);
         setFiles(files);
@@ -18,7 +21,18 @@ const DragAndDrop = () => {
             onError={console.error}
             src={files}
         >
-            <DropzoneEmptyState />
+            <DropzoneEmptyState >
+                <div className="flex size-8 items-center justify-center rounded-md bg-muted text-muted-foreground">
+                    <UploadIcon size={16} />
+                </div>
+                <p className="my-2 w-full truncate text-wrap font-medium text-sm">
+                    {t.DRAG_AND_DROP.TITLE}
+                </p>
+                <p className="w-full truncate text-wrap text-muted-foreground text-xs">
+                    {t.DRAG_AND_DROP.HOW_TO_UPLOAD_TEXT}
+                </p>
+                <p className="text-wrap text-muted-foreground text-xs">{t.DRAG_AND_DROP.ALLOWED_IMAGE}</p>
+            </DropzoneEmptyState >
             <DropzoneContent />
         </Dropzone>
     );
